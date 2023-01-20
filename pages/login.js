5beb0619e64c96859d13a43a5bee79f339d62b3b
houariDz95 <houarieddrief7@gmail.com>
2023-01-20 12:49:28 +0100

    add link

--- pages/login.js
+++ pages/login.js
@@ -11,7 +11,7 @@ const Login = () => {
   const router = useRouter()
   const handelClick = async () => {
     try{
-     const res =await axios.post('http://localhost:3000/api/auth/login', {
+     const res =await axios.post('https://nova-dashboard.vercel.app/api/auth/login', {
         username,
         password
       })
@@ -29,8 +29,8 @@ const Login = () => {
         <div className="h-[80%] w-full flex items-center justify-center">
         <form className="w-[320px] h-[280px] p-4 flex flex-col dark:bg-main-bg bg-main-dark-bg items-center gap-8">
           <h1 className="text-2xl font-bold flex items-center dark:text-[#20232A] text-white "> <SlScreenSmartphone size={30} className="text-cyan-500"/> NovaPhone</h1>
-          <input className="border-b-1 text-white border-gray-500 w-full focus:outline-none bg-transparent p-2" type="text" placeholder="username"value={username} onChange={(e) => setUsername(e.target.value)}/>
-          <input className="border-b-1 text-white border-gray-500 w-full focus:outline-none bg-transparent p-2"  type="password" placeholder="password"value={password} onChange={(e) => setPassword(e.target.value)}/>
+          <input className="border-b-1 border-gray-500 w-full focus:outline-none bg-transparent p-2" type="text" placeholder="username"value={username} onChange={(e) => setUsername(e.target.value)}/>
+          <input className="border-b-1  border-gray-500 w-full focus:outline-none bg-transparent p-2"  type="password" placeholder="password"value={password} onChange={(e) => setPassword(e.target.value)}/>
           <button type="button" className="bg-cyan-500 px-10 py-2 cursor-pointer text-white text-md font-semibomd rounded-sm" onClick={handelClick}>Login</button>
           {error && <span className="text-red-500">Wrong Credentials</span>}
         </form>
